3116f9401d34115d48425c6f70d7fa93d6a62c28
LazarIsailovic <isailovic00@gmail.com>
2022-06-15 17:22:20 +0200

    Revert "Add files via upload"
    This reverts commit c337cab37a83ac153303bad4152f841bd0f57d78.

--- Softver.java
+++ Softver.java
@@ -7,53 +7,11 @@ public class Softver {
 	
 
 	private String Naziv;
-	private Cetkica cetkice;
-	private String Alat;
-	public String getNaziv() {
-		return Naziv;
-	}
-	public void setNaziv(String naziv) {
-		Naziv = naziv;
-	}
-	public Cetkica getCetkice() {
-		return cetkice;
-	}
-	public void setCetkice(Cetkica cetkice) {
-		this.cetkice = cetkice;
-	}
-	public String getAlat() {
-		return Alat;
-	}
-	public void setAlat(String alat) {
-		Alat = alat;
-	}
-	public String getFormati() {
-		return Formati;
-	}
-	public void setFormati(String formati) {
-		Formati = formati;
-	}
-	public Render getRender() {
-		return render;
-	}
-	public void setRender(Render render) {
-		this.render = render;
-	}
-	public Softver(String naziv, Cetkica cetkice, String alat, String formati, Render render) {
-		super();
-		Naziv = naziv;
-		this.cetkice = cetkice;
-		Alat = alat;
-		Formati = formati;
-		this.render = render;
-	}
+	private List<Cetkica> cetkice=new ArrayList<>();
+	private String Modifikatori;
 	private String Formati;
 	private Render render;
-	@Override
-	public String toString() {
-		return Naziv +" "+ Formati+" " + Alat+" "+cetkice.toString()+" " +render.toString();
-	}
-
+	//alati za animaciju?
 	
 
 }
